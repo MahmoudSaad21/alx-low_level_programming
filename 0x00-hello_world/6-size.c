59731d3419e3f09b7f0bdacc078084116f936123
Mahmoud Saad <mahm0udsa3d404@gmail.com>
2023-03-16 15:58:00 -0700

    sizeof

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+*main - Main function of my project
+*
+*
+*Return: return 0 everytime
+*/
+int main(void)
+{
+  printf("Size of a char: %d byte(s)\n", sizeof(char),"Size of an int: %d byte(s)\n", sizeof(int),"Size of a long int: %d byte(s)\n", sizeof(long int),"Size of a long long int: %d byte(s)\n", sizeof(long long int),printf("Size of a float: %d byte(s)\n", sizeof(float));
+return (0);
+}
