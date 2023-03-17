6d852580e15e41439aba74aec42d4aa1377bb35b
Mahmoud Saad <mahm0udsa3d404@gmail.com>
2023-03-17 10:43:03 -0700

    for

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+*main - Main function of my project
+*
+*
+*Return: return 0 everytime
+*/
+int main(void)
+{
+int c;
+for (c = 0; c <= 9; c++)
+putchar(c);
+putchar('\n');
+return (0);
+}
