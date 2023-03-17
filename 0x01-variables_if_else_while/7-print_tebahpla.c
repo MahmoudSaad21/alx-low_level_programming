58aa9e9372a3eaf1e2309678148c4b4821fb489d
Mahmoud Saad <mahm0udsa3d404@gmail.com>
2023-03-17 16:11:28 -0700

    putchar reverse

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,17 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+*main - Main function of my project
+*
+*
+*Return: return 0 everytime
+*/
+int main(void)
+{
+int ch;
+for (ch = 'z'; ch >= 'a'; ch--)
+putchar(ch);
+putchar('\n');
+return (0);
+}
