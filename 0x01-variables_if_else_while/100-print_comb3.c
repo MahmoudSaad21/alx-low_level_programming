d9b5036b0d879b57ec83a4c542196e1e3c7f3f47
Mahmoud Saad <mahm0udsa3d404@gmail.com>
2023-03-17 16:35:13 -0700

    two for

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,29 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+*main - Main function of my project
+*
+*
+*Return: return 0 everytime
+*/
+int main(void)
+{
+int ch1,ch2;
+for (ch1 = '0'; ch1 <= '9'; ch1++)
+{
+for (ch2 = '0'; ch2 <= '9'; ch2++)
+if (ch1 < ch2)
+{
+putchar(ch1);
+putchar(ch2);
+if (ch1 != '8')
+{
+putchar(',');
+putchar(' ');
+}
+}
+}
+putchar('\n');
+return (0);
+}
