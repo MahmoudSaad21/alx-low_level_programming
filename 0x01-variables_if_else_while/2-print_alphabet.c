d91eae1a278ef44d47294e5e9a88ae52ec721b4c
Mahmoud Saad <mahm0udsa3d404@gmail.com>
2023-03-17 10:08:15 -0700

    for loop

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,16 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+*main - Main function of my project
+*
+*
+*Return: return 0 everytime
+*/
+int main(void)
+{
+for (char c = 'a'; c <= 'z'; c++)
+putchar(c);
+putchar('\n');
+return (0);
+}
