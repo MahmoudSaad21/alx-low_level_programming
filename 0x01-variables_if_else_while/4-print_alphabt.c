64ed60843d4a3ab1fc909651f9079e4670d7841a
Mahmoud Saad <mahm0udsa3d404@gmail.com>
2023-03-17 10:35:55 -0700

    for and if

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,20 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+*main - Main function of my project
+*
+*
+*Return: return 0 everytime
+*/
+int main(void)
+{
+char c;
+for (c = 'a'; c <= 'z'; c++){
+if (c=='e' ||c=='q')
+continue;
+else
+putchar(c);}
+putchar('\n');
+return (0);
+}
