c2cdb433952c1e6b28b1a201e1db484e4fd71544
Mahmoud Saad <mahm0udsa3d404@gmail.com>
2023-03-17 10:39:38 -0700

    for and if

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -10,11 +10,13 @@
 int main(void)
 {
 char c;
-for (c = 'a'; c <= 'z'; c++){
-if (c=='e' ||c=='q')
+for (c = 'a'; c <= 'z'; c++)
+{
+if (c == 'e' || c == 'q')
 continue;
 else
-putchar(c);}
+putchar(c);
+}
 putchar('\n');
 return (0);
 }
