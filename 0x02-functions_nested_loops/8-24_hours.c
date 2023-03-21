c46112bbcf8fd3466b5c6984894fd09c4b0c6586
Mahmoud Saad <mahm0udsa3d404@gmail.com>
2023-03-21 12:24:44 -0700

    jack

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -11,16 +11,16 @@ for (count1 = 0; count1 <=2; count1++)
 {
 for (count2 = 0; count2 <=9; count2++)
 {
-_putchar(count1 + 48)
-_putchar(count2 + 48)
-_putchar(58)
+_putchar(count1 + 48);
+_putchar(count2 + 48);
+_putchar(58);
 for (count3 = 0; count3 <=5; count3++)
 {
 for (count4 = 0; count4 <=9; count4++)
 {
-_putchar(count3 + 48)
-_putchar(count4 + 48)
-_putchar(10)
+_putchar(count3 + 48);
+_putchar(count4 + 48);
+_putchar(10);
 }
 }
 }
