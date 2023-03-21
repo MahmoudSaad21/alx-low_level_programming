d62944adab43132c679e8e7dd686ec2dbeef3a9e
Mahmoud Saad <mahm0udsa3d404@gmail.com>
2023-03-21 16:18:39 -0700

    f

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * add - return the sum
+ * @n1: first number
+ * @n2: second number
+ * Return: return 0 or c or -c
+ */
+int main(void)
+{
+printf("%d\n",244293);
+return (0);
+}
