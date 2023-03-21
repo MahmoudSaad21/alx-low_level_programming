b5a62e00de5dbdd94dbabd369d8a1e21ad951725
Mahmoud Saad <mahm0udsa3d404@gmail.com>
2023-03-21 13:48:43 -0700

    times

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -12,21 +12,14 @@ for (count1 = 0; count1 <= 9 ; count1++)
 for (count2 = 0; count2 <= 9 ; count2++)
 {
 int temp = count1 * count2;
-if (temp < 10 )
-{
 _putchar(temp + '0');
 _putchar(',');
 _putchar(' ');
+if (temp <= 9)
 _putchar(' ');
-
-}
 else
-{
 _putchar((temp / 10) + '0');
 _putchar((temp % 10) + '0');
-_putchar(',');
-_putchar(' ');
-}
 }
 _putchar(10);
 }
