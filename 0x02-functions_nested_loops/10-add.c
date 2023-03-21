ded805a7abf52f2f6458307bab32432ce6649459
Mahmoud Saad <mahm0udsa3d404@gmail.com>
2023-03-21 15:03:41 -0700

    add

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -2,10 +2,10 @@
 /**
  * add - return the sum
  * @n1: first number
- * @n2: second number 
+ * @n2: second number
  * Return: return 0 or c or -c
  */
 int add(int n1, int n2)
 {
-  return (n1 + n2);
+return (n1 + n2);
 }
