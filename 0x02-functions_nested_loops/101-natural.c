8f15bf686a984c7172ff64b90a38f4f5826e6c21
Mahmoud Saad <mahm0udsa3d404@gmail.com>
2023-03-21 16:28:33 -0700

    sum

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -9,14 +9,14 @@ int main(void)
 int n, sum = 0;
 for (n = 0; n < 1024; n = n + 3)
 {
-if(n < 1024 && n % 5 != 0)
+if (n < 1024 && n % 5 != 0)
 sum += n;
 }
 for (n = 0; n < 1024; n = n + 5)
 {
-if(n<1024)
+if (n < 1024)
 sum += n;
 }
-printf("%d \n", sum);
+printf("%d\n", sum);
 return (0);
 }
