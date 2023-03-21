734f599d2ffed46ed613b52916e75877444b4f1b
Mahmoud Saad <mahm0udsa3d404@gmail.com>
2023-03-21 15:01:57 -0700

    add

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -1,10 +1,11 @@
 #include "main.h"
 /**
- * add - return the last digit
- *
+ * add - return the sum
+ * @n1: first number
+ * @n2: second number 
  * Return: return 0 or c or -c
  */
 int add(int n1, int n2)
 {
-  return (n1+n2);
+  return (n1 + n2);
 }
