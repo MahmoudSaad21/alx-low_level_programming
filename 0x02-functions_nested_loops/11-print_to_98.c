8c330e4e93da5afcd28c7c658c8abe678a57576e
Mahmoud Saad <mahm0udsa3d404@gmail.com>
2023-03-21 15:22:13 -0700

    print to

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,30 @@
+#include "main.h"
+/**
+ * print_to_98 - return the last digit
+ *@c: character to be verified
+ *Return: return 0 or c or -c
+ */
+void print_to_98(int c)
+{
+int count;
+if (c >= 98)
+{
+for (count = c ;count <= 98;count++)
+{
+_putchar(temp + '0');
+_putchar(',');
+_putchar(' ');
+}
+_putchar(10);
+}
+else
+{
+for (count = c ;count >= 98;count--)
+{
+_putchar(temp + '0');
+_putchar(',');
+_putchar(' ');
+}
+_putchar(10);
+}
+}
