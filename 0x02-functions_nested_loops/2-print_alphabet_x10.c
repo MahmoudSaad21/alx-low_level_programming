33c87e0b63173044bde154023dee694f38d4a971
Mahmoud Saad <mahm0udsa3d404@gmail.com>
2023-03-21 10:31:57 -0700

    print_alphabet_x10

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+*print_alphabet - function that print alphabet
+*
+*
+*Return: return 0 everytime
+*/
+void print_alphabet_x10(void)
+{
+int count;
+int count1;
+for (count = 0; count <= 9; count++)
+for (count = 'a'; count <= 'z'; count++)
+{
+_putchar(count);
+}
+_putchar(10);
+}
+}
