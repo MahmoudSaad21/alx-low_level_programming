0986dde9b75c49f824b3da58cb88b9ede527a23d
Mahmoud Saad <mahm0udsa3d404@gmail.com>
2023-03-21 10:21:22 -0700

    alphabet

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,5 +1,5 @@
 #include "main.h"
-11;rgb:0000/0000/0000/**
+/**
 *main - Main function of my project
 *
 *
@@ -13,5 +13,4 @@ for (count = 'a'; count <= 'z'; count++)
 _putchar(count);
 }
 _putchar(10);
-return (0);
 }
