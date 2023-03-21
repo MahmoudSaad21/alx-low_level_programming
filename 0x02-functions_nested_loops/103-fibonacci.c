576ad5102714befa1a56a4b612494d926f1c2afe
Mahmoud Saad <mahm0udsa3d404@gmail.com>
2023-03-21 16:44:22 -0700

    f

---
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+/**
+*main - prints out first 50
+*fibonacci suit numbers
+*Return: return 0
+*/
+int main(void)
+{
+int count;
+ unsigned long n1 = 0, n2 = 1, n3, sum = 0;
+for (count = 0; count < 40000; count++)
+{
+if(n3 >= 4000000)
+break;
+n3 = n1 + n2;
+n1 = n2;
+n2 = n3;
+if (n1 % 2 == 0)
+sum = sum +n1;
+}
+ 
+printf("%ld\n", sum);
+return (0);
+}
---
+++ 0x02-functions_nested_loops/103-fibonacci.c~
@@ -0,0 +1,23 @@
+#include <stdio.h>
+/**
+*main - prints out first 50
+*fibonacci suit numbers
+*Return: return 0
+*/
+int main(void)
+{
+int count;
+unsigned long n1 = 0, n2 = 1, n3;
+for (count = 0; count < 50; count++)
+{
+n3 = n1 + n2;
+printf("%lu", n3);
+n1 = n2;
+n2 = n3;
+if (count == 49)
+printf("\n");
+else
+printf(", ");
+}
+return (0);
+}
