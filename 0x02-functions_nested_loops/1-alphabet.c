2e94f73918526b319fa23186dd46f536f286416a
Mahmoud Saad <mahm0udsa3d404@gmail.com>
2023-03-21 10:17:31 -0700

    alphabet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+*main - Main function of my project
+*
+*
+*Return: return 0 everytime
+*/
+void print_alphabet(void)
+{
+int count;
+for (count = 'a'; count <= 'z'; count++)
+{
+_putchar(count);
+}
+_putchar("\n");
+return (0);
+}
