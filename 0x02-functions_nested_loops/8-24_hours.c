dbda41c514d7a8d8825086333797b0017d53aefb
Mahmoud Saad <mahm0udsa3d404@gmail.com>
2023-03-21 12:42:17 -0700

    jack

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -7,13 +7,13 @@
 void jack_bauer(void)
 {
 int count1, count2, count3, count4;
-for (count1 = 0; count1 <=2 ; count1++)
+for (count1 = 0; count1 <= 2 ; count1++)
 {
-for (count2 = 0; count2 <=3 ; count2++)
+for (count2 = 0; count2 <= 3 ; count2++)
 {
-for (count3 = 0; count3 <=5 ; count3++)
+for (count3 = 0; count3 <= 5 ; count3++)
 {
-for (count4 = 0; count4 <=9 ; count4++)
+for (count4 = 0; count4 <= 9 ; count4++)
 {
 _putchar(count1 + 48);
 _putchar(count2 + 48);
@@ -21,7 +21,7 @@ _putchar(58);
 _putchar(count3 + 48);
 _putchar(count4 + 48);
 _putchar(10);
-if (count1 == 2 && count2 ==3)
+if (count1 == 2 && count2 == 3 && count3 == 5 && count4 == 9)
 return;
 } 
 }
