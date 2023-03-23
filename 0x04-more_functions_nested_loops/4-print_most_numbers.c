ca5ea5ca1de878631d0365a8c4cbe92062fbb403
Mahmoud Saad <mahm0udsa3d404@gmail.com>
2023-03-23 10:50:34 -0700

    print_most_numbers

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -2,7 +2,6 @@
 /**
 * print_most_numbers - function that print alphabet 10 times
 *
-*
 *Return: return 0 everytime
 */
 void print_most_numbers(void)
@@ -10,7 +9,7 @@ void print_most_numbers(void)
 int n = 0;
 for (n = 48; n <= 57; n++)
 {
-if (n !=50 && n !=52)
+if (n != 50 && n != 52)
 {
 _putchar(n);
 }
