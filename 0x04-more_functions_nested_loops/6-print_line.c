a2378b34ca245e5c5dbe34ae0abd760c9f82fc3c
Mahmoud Saad <mahm0udsa3d404@gmail.com>
2023-03-23 11:23:25 -0700

    print_line

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+* print_line - function that print numbers 10 times
+*
+*Return: return 0 everytime
+*/
+void print_line(int n)
+{
+int count ;
+if (n > 0)
+for (count = 0; count < n; count++)
+_putchar(95);
+_putchar(10);
+}
