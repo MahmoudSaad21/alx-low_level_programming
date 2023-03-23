6ccc7a931e924b7a746f5d08ed49568e69f04fa9
Mahmoud Saad <mahm0udsa3d404@gmail.com>
2023-03-23 10:24:46 -0700

    print_numbers

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+*print_alphabet_x10 - function that print alphabet 10 times
+*
+*
+*Return: return 0 everytime
+*/
+void print_alphabet_x10(void)
+{
+int count1;
+for (count1 = 0; count1 <= 9; count1++)
+_putchar(count);
+_putchar(10);
+}
