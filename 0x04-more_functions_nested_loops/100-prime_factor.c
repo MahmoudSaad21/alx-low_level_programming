5fef2732bdc00619b26acc17e9b72e3a950dd28e
Mahmoud Saad <mahm0udsa3d404@gmail.com>
2023-03-23 19:24:55 -0700

    ..

--- 0x04-more_functions_nested_loops/100-prime_factor.c
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -8,13 +8,14 @@
 */
 int main(void)
 {
-long long n = 612852475143;
-long long max_prime = -1;
+long n = 612852475143;
+long max_prime = -1;
+long i;
 while (n % 2 == 0) {
 max_prime = 2;
-n /= 2;
-}
-for (int i = 3; i <= sqrt(n); i += 2) {
+n/= 2;
+} 
+for (i = 3; i <= sqrt(n); i += 2) {
 while (n % i == 0) {
 max_prime = i;
 n /= i;
@@ -23,6 +24,6 @@ n /= i;
 if (n > 2) {
 max_prime = n;
 }
-printf("%lld\n", max_prime);
+printf("%ld\n", max_prime);
 return (0);
 }
