3b9b6b6531483037b7180129ea7bf97db5f12d05
Mahmoud Saad <mahm0udsa3d404@gmail.com>
2023-03-23 18:23:00 -0700

    fizz_buzz

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,25 @@
+#include "main.h"
+#include <stdio.h>
+/**
+* main -  prints a square
+*
+*Return: return nothing
+*/
+int main(void)
+{
+int count1;
+for (count1 = 1; count1 <= 100; count1++)
+{
+if (count1 % 3 == 0 && count1 % 5 == 0)
+printf("FizzBuzz");
+else if (count1 % 3 == 0 )
+printf("Fizz");
+else if (count1 % 5 == 0 )
+printf("Buzz");
+else
+printf(count1);
+printf(" ")
+}
+_putchar(10);
+return (0)
+}
