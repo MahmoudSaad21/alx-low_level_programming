f0b62e3351e3c490e353ba9c48edd018249aecad
Mahmoud Saad <mahm0udsa3d404@gmail.com>
2023-03-23 19:42:51 -0700

    print_remaining_days

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -3,16 +3,18 @@
 #include <math.h>
 /**
 * print_number -  prints a number
-*@n : is the number
-*Return: return nothing
+* @n: is the number
+* Return: void
 */
 void print_number(int n)
 {
-if (n < 0) {
+if (n < 0)
+{
 _putchar('-');
 n = -n;
 }
-if (n / 10) {
+if (n / 10)
+{
 print_number(n / 10);
 }
 _putchar(n % 10 + '0');
