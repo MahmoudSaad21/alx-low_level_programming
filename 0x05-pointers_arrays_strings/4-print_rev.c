e3b779cb870ce46c9f016e92598e7197d4fd0a10
Mahmoud Saad <mahm0udsa3d404@gmail.com>
2023-03-27 18:52:32 -0700

    print_rev

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -6,13 +6,11 @@
 */
 void print_rev(char *s)
 {
-int len, count = 0;
-int len = 0;
+int len = 0, count = 0;
 while (s[len] != '\0')
 len++;
 for (count = len - 1; count >= 0; count--)
 _putchar(s[count]);
-
 _putchar(10);
 }
 }
