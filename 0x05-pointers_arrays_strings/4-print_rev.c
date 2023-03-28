7fdb9f6db9abd27cda05fa41ce6ead5aec2a5f52
Mahmoud Saad <mahm0udsa3d404@gmail.com>
2023-03-27 18:51:07 -0700

    print_rev

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+*print_rev - prints a string, in reverse
+*@s: first string
+*Return: nothing
+*/
+void print_rev(char *s)
+{
+int len, count = 0;
+int len = 0;
+while (s[len] != '\0')
+len++;
+for (count = len - 1; count >= 0; count--)
+_putchar(s[count]);
+
+_putchar(10);
+}
+}
