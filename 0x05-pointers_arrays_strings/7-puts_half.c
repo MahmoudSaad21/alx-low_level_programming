0dbb7ea029c79ffd0c963b4ed72c8a3aa3df9ce7
Mahmoud Saad <mahm0udsa3d404@gmail.com>
2023-03-27 19:26:22 -0700

    puts_half'

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -9,7 +9,11 @@ void puts_half(char *s)
 int len = 0, count = 0;
 while (s[len] != '\0')
 len++;
-for (count = len/2; count < len; count++)
+if (len % 2 == 0)
+count = len / 2;
+else
+count = (len + 1) / 2;
+for (; count < len; count++)
 _putchar(s[count]);
 _putchar(10);
 }
