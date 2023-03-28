404cc691e925ed6724e71d04133291cd3c3e3256
Mahmoud Saad <mahm0udsa3d404@gmail.com>
2023-03-27 18:23:53 -0700

    _strlen

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+*_strlen - swap two numbers
+*@s: first number
+*Return : the length
+*/
+int _strlen(char *s)
+{
+int len = 0;
+while (str[len] != '\0') {
+len++;
+}
+return len;
+}
