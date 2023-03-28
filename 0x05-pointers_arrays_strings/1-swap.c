3651912421c7fe67046b86e34a47e4eae90ed80b
Mahmoud Saad <mahm0udsa3d404@gmail.com>
2023-03-27 18:13:54 -0700

    swap_int

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+*reset_to_98 - reset value of an integer to 98
+*@a: first number
+*@b: second number
+*Return : nothing
+*/
+void swap_int(int *a, int *b)
+{
+int x = *a;
+*a = *b;
+*b = x;
+}
