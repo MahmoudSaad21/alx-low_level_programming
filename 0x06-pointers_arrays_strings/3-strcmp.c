d835e27ed23e938e312579641ff0f3f6a7e53efa
Mahmoud Saad <mahm0udsa3d404@gmail.com>
2023-03-30 16:31:04 -0700

    _strcmp

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,34 @@
+#include "main.h"
+/**
+ *_strcmp - compare two strings
+ *@s1: first string
+ *@s2: second string
+ *Return: returns 
+ */
+int _strcmp(char *s1, char *s2);
+{
+int index = 0, d = 0;
+while (True)
+{
+if (s1[index] == '\0' && s2[index] == '\0')
+break;
+else if (s1[index] == '\0')
+{
+d = s2[index];
+break;
+}
+else if (s2[index] == '\0')
+{
+d = s1[index];
+break;
+}
+else if (s1[index] != s2[index])
+{
+d = s1[index] - s2[index];
+break;
+}
+else
+index++;
+}
+return (d);
+}
