30faec392f89d3bbaef52eb4db6dc0bbb26f17ef
Mahmoud Saad <mahm0udsa3d404@gmail.com>
2023-03-30 15:37:40 -0700

    ..

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -1,11 +1,11 @@
 #include "main.h"
 /**
- *_strcat - concatenates  two strings 
- *@dest: String that will be appended
- *@src: String to be concatenated 
- *
- * Return: returns poiner to dest
- */
+*_strcat - concatenates two strings 
+*@dest: String that will be appended
+*@src: String to be concatenated
+*
+*Return: returns poiner to dest
+*/
 char *_strcat(char *dest, char *src)
 {
 int index = 0, dest_len = 0;
