a1b0f331e25af7f6c1689bab0c6bef4d8fde77e2
Mahmoud Saad <mahm0udsa3d404@gmail.com>
2023-03-30 16:44:40 -0700

    strcmp

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -1,10 +1,12 @@
 #include "main.h"
+
 /**
 * _strcmp - compare two strings
 *@s1: first string
 *@s2: second string
 *Return: returns 
 */
+
 int _strcmp(char *s1, char *s2)
 {
 int index = 0, d = 0;
