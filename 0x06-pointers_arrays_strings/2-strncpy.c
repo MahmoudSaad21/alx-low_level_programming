133206477b48ba6e33969a769e3fdc43fa4742c4
Mahmoud Saad <mahm0udsa3d404@gmail.com>
2023-03-30 16:15:55 -0700

    _strncpy

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -1,14 +1,14 @@
 #include "main.h"
 /**
- *_strncay - copy two strings
+ *_strncpy - copy two strings
  *@dest: string to be appended upon
  *@src: string to be completed at end of dest
  *@n:integer parameter
  *Return: returns new concatenated string
-*/
-char *_strncay(char *dest, char *src, int n)
+ */
+char *_strncpy(char *dest, char *src, int n)
 {
-int index = 0;
+int index;
 for (index = 0; src[index] != '\0' && index < n; index++)
 dest[index] = src[index];
 while (index < n)
