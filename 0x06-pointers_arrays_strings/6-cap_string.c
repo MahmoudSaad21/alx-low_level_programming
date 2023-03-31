2c60bc806a94a46f9c5b0e13a904b14e429e8eb5
Mahmoud Saad <mahm0udsa3d404@gmail.com>
2023-03-30 17:20:53 -0700

    cap_stringcap

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -10,12 +10,8 @@ int i = 0;
 while (str[i] != '\0')
 {
 if (i == 0 || str[i - 1] == ' ' || str[i - 1] == '\t' || str[i - 1] == '\n' || str[i - 1] == ',' || str[i - 1] == ';' || str[i - 1] == '.' || str[i - 1] == '!' || str[i - 1] == '?' || str[i - 1] == '"' ||str[i - 1] == '(' || str[i - 1] == ')' || str[i - 1] == '{' || str[i - 1] == '}')
-{
 if (str[i] >= 'a' && str[i] <= 'z')
-{
 str[i] = str[i] - 'a' + 'A';
-}
-}
 i++;
 }
 return (str);
