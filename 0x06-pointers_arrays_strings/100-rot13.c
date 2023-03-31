fd9e4aa50e5f275271f4a2ebb66baa6f032ea82c
Mahmoud Saad <mahm0udsa3d404@gmail.com>
2023-03-30 18:28:55 -0700

    ..

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -2,22 +2,26 @@
 
 /**
  * rot13 -  a   function that encodes a string using rot13.
- * @str: An input string to encode using rot13
+ * @s: An input string to encode using rot13
  * Return: An encode string
  */
-char *rot13(char *str)
+char *rot13(char *s)
 {
-int i;
-char c;
-char *result = str;
-for (i = 0; str[i] != '\0'; i++)
-{
-c = str[i];
-if (c >= 'a' && c <= 'z') 
-c = 'a' + ((c - 'a' + 13) % 26);
-else if (c >= 'A' && c <= 'Z')
-c = 'A' + ((c - 'A' + 13) % 26);
-result[i] = c;
-}
-return (result);
+	int i = 0;
+
+	while (s[i] != '\0')
+	{
+		while ((s[i] >= 'a' && s[i] <= 'z') ||
+				(s[i] >= 'A' && s[i] <= 'Z'))
+		{
+			if ((s[i] >= 'a' && s[i] <= 'm') ||
+					(s[i] >= 'A' && s[i] <= 'M'))
+				s[i] += 13;
+			else
+				s[i] -= 13;
+			i++;
+		}
+		i++;
+	}
+	return (s);
 }
