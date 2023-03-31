b1f862c8e2494cf9439185abd0a5928483c94f19
Mahmoud Saad <mahm0udsa3d404@gmail.com>
2023-03-30 17:33:02 -0700

    cap_string

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -9,7 +9,7 @@ char *cap_string(char *str)
 int index = 0;
 while (str[index] != '\0')
 {
-if (i = 0 || str[index - 1] == ' ' ||
+if (index = 0 || str[index - 1] == ' ' ||
 str[index - 1] == '\t' ||
 str[index - 1] == '\n' ||
 str[index - 1] == ',' ||
