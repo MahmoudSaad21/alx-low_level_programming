cb6435d36e41b275d4e7738fed08e66ddb256944
Mahmoud Saad <mahm0udsa3d404@gmail.com>
2023-04-03 15:07:18 -0700

    _memcpy

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+*_memcpy - copy the constant byte b
+*@dest:target
+*@src: constant byte
+*@n:number of byte
+*Return: returns new value of target
+*/
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+unsigned int i;
+for (i = 0; i < n; i++)
+dest[i] = src[i];
+return (s);
+}
