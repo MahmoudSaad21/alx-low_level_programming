6672a8aa1a046ac4a3e218c60fa0fd79bb64fe70
Mahmoud Saad <mahm0udsa3d404@gmail.com>
2023-04-05 15:10:11 -0700

    is_prime_number

---
+++ 0x08-recursion/6-is_prime_number.c
@@ -0,0 +1,27 @@
+#include "main.h"
+/**
+*is_prime_number - see if the number is prime or not
+*@n:target
+*Return: returns the value
+*/
+int is_prime_number(int n)
+{
+if (n <= 0)
+return (0);
+else
+return (is_prime_number_help(n / 2, n));
+}
+/**
+*is_prime_number_help - returns the square root of a number
+*@x: test number
+*@n: squared number
+*Return: the square root of n
+*/
+int is_prime_number_help(int x, int n)
+{
+if (x <= 1)
+return (1);
+else if (n % x == 0)
+return (0);
+return (is_prime_number_help(x - 1, n));
+}
