d2d0127cce27b960c52d3eca8be6d6af3c67d980
Mahmoud Saad <mahm0udsa3d404@gmail.com>
2023-04-10 14:13:29 -0700

    ..

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+* main - prints program name
+*@argc: number of arguments
+*@argv: array of arguments
+*Return: 0
+*/
+int main(int argc, char *argv[])
+{
+int i;
+for (i = 0;i < argc;i++0)
+{
+printf("%d\n", argv[i]);
+}
+return (0);
+}
