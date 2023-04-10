9a7c349978e7a87824a6405c8a6a6ca2d0f7ff94
Mahmoud Saad <mahm0udsa3d404@gmail.com>
2023-04-10 08:41:24 -0700

    _strspn

---
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,27 @@
+#include "main.h"
+/**
+*_strspn - search the number of bytes in the initial
+*segment of s which consist only of bytes from accept
+*@s:target
+*@accept:byte
+*Return: returns new value of target
+*/
+unsigned int _strspn(char *s, char *accept)
+{
+int i = 0, j = 0;
+unsigned int n = 0;
+while (s[i] != '\0')
+{
+while(accept[j] != '\0')
+{
+if (accept[j] == c)
+{
+n++;
+break;
+}
+j++;
+}
+i++
+}
+return (n);
+}
