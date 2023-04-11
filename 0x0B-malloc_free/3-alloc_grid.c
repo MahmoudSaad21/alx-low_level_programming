16d3979ed3ed60f31bd7e24cb6704edafa60ff19
Mahmoud Saad <mahm0udsa3d404@gmail.com>
2023-04-11 15:06:22 -0700

    ..

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -8,7 +8,8 @@
 */
 int **alloc_grid(int width, int height)
 {
-int **array, i, j;
+int **array;
+int i, j;
 if (width <= 0 || height <= 0)
 return (NULL);
 array = (int **)malloc(height * sizeof(int *));
