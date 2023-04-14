6d840c485138afab6f03d9b52116fb49231b0247
Mahmoud Saad <mahm0udsa3d404@gmail.com>
2023-04-13 18:44:21 -0700

    ..

---
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -0,0 +1,30 @@
+#include "main.h"
+#include <stddef.h>
+#include <stdlib.h>
+/**
+* array_range - creates an array of integers
+* @min: smallest number in the array
+* @max: lagrest value in the array
+*
+* Return: pointer to the address of the memory block
+*/
+
+int *array_range(int min, int max)
+{
+int *temp;
+int index, index2 = 0;
+if (min > max)
+return (NULL);
+temp = malloc(sizeof(*temp) * ((max - min) + 1));
+if (block == NULL)
+return (NULL);
+else
+{
+for (index = min; index <= max; index++)
+{
+temp[index2] = index;
+index2++;
+}
+return (temp);
+}
+}
