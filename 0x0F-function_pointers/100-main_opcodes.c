392014d6bcee2174214a61a3f059791dc7076423
Mahmoud Saad <mahm0udsa3d404@gmail.com>
2023-04-19 15:49:47 -0700

    ..

---
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+*main - prints the opcodes of its own main function
+*@argc: number of argument
+*@argv: array of argument
+*Return: 0 on success,1 on incorrect number of arguments,2 on negativenumber
+*/
+int main(int argc, char **argv)
+{
+int i, n;
+unsigned char *p = (unsigned char *) main;
+if (argc != 2)
+{
+printf("Error\n");
+return (1);
+}
+n = atoi(argv[1]);
+if (n < 0)
+{
+printf("Error\n");
+return (2);
+}
+for (i = 0; i < n; i++)
+printf("%02x ", *(p + i));
+printf("\n");
+return 0;
+}
