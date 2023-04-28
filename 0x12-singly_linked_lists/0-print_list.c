3a40ab1c326bf749b36db92c253ba7013df949a9
MahmoudSaad21 <mahm0udsa3d404@gmail.com>
2023-04-28 02:23:28 -0700

    ..

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -8,13 +8,14 @@
 size_t print_list(const list_t *h)
 {
 size_t c = 0;
-while (h != NULL)
+const list_t *temp = h;
+while (temp != NULL)
 {
-if (h->str == NULL)
+if (temp->str == NULL)
 printf("[0] (nil)\n");
 else
-printf("[%d] %s\n", h->len, h->str);
-h = h->next;
+printf("[%d] %s\n", temp->len, temp->str);
+temp = temp->next;
 c++;
 }
 return (c);
