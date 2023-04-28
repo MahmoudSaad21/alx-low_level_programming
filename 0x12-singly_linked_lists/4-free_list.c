eb49c3b614aeabc8800555cdf7c04da76e76d883
MahmoudSaad21 <mahm0udsa3d404@gmail.com>
2023-04-28 04:01:01 -0700

    ..

---
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "lists.h"
+/**
+*free_list - Frees a linked list
+*@head: Pointer to the head node of the list
+*Return: nothing
+*/
+void free_list(list_t *head)
+{
+list_t *current_node;
+while (head != NULL)
+{
+current_node = head;
+head = head->next;
+free(current_node->data);
+free(current_node);
+}
+}
