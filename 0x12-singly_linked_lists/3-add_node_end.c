fe4b28f3bb9e189cd8b3cbc2d397a7075ec37fc7
MahmoudSaad21 <mahm0udsa3d404@gmail.com>
2023-04-28 03:54:22 -0700

    ..

--- 0x12-singly_linked_lists/3-add_node_end.c
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -14,7 +14,7 @@ list_t *new_node, *current_node;
 new_node = malloc(sizeof(list_t));
 if (new_node == NULL)
 return (NULL);
-ew_node->str = strdup(str);
+new_node->str = strdup(str);
 if (new_node->str == NULL)
 {
 free(new_node);
