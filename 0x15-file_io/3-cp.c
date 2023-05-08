7807dde2cfd929d1b61251771c676519cbf05b2c
MahmoudSaad21 <mahm0udsa3d404@gmail.com>
2023-05-08 13:57:36 -0700

    ..

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -4,6 +4,46 @@
 #include <fcntl.h>
 #include <errno.h>
 /**
+*_97 - error 97
+*@argv: An array of pointers to the arguments
+*Return: nothing
+*/
+void _97 ()
+{
+dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
+exit(97);
+}
+/**
+*_98 - error 98
+*@argv: An array of pointers to the arguments
+*Return: nothing
+*/
+void _98 (char *argv[])
+{
+dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
+exit(98);
+}
+/**
+*_99 - error 99
+*@argv: An array of pointers to the arguments
+*Return: nothing
+*/
+void _99 (char *argv[])
+{
+dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
+exit(99);
+}
+/**
+*_100 - error 100
+*@fd: the file
+*Return: nothing
+*/
+void _100 (int fd)
+{
+dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd);
+exit(100);
+}
+/**
 *main - The entry point of the program
 *@argc: The number of arguments passed to the program
 *@argv: An array of pointers to the arguments
@@ -14,45 +54,24 @@ int main(int argc, char *argv[])
 int fd_from, fd_to, read_size, write_size;
 char buffer[1024];
 if (argc != 3)
-{
-dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
-exit(97);
-}
+_97(argv);
 fd_from = open(argv[1], O_RDONLY);
 if (fd_from == -1)
-{
-dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
-exit(98);
-}
+_98(argv);
 fd_to = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, 0664);
 if (fd_to == -1)
-{
-dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
-exit(99);
-}
+_99(argv);
 while ((read_size = read(fd_from, buffer, 1024)) > 0)
 {
 write_size = write(fd_to, buffer, read_size);
 if (write_size != read_size)
-{
-dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
-exit(99);
-}
+_99(argv);
 }
 if (read_size == -1)
-{
-dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
-exit(98);
-}
+_98(argv);
 if (close(fd_from) == -1)
-{
-dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd_from);
-exit(100);
-}
+_100(fd_from);
 if (close(fd_to) == -1)
-{
-dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd_to);
-exit(100);
-}
+_100(fd_to);
 return (0);
 }
